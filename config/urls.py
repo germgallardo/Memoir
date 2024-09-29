532ceb0b5f6fc4b037f843dbb70950be6de2c923
germgallardo <german.mgallardo@gmail.com>
2024-09-30 01:08:42 +0200

    updating templates

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import home
+from core.views import home, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
+    path('profile/', profile, name='profile'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'core/template_core.html', {})
+    return render(request, 'core/core.html', {})
+
+def profile(request):
+    context = {
+        'fav_shows' : ["Lost", "Mr.Robot", "Aquí no hay quien viva"]
+    }
+    return render(request, 'core/profile.html', context)
